e11db0be0b96aed968653c3ae0877a88e1c56a12
Lucas <lucasnguyenotlk@outlook.com>
2025-03-14 10:05:42 +1000

    fixed indentation of ToDoItem class

--- src/ToDoItem.java
+++ src/ToDoItem.java
@@ -1,35 +1,33 @@
 public class ToDoItem {
-    public class ToDoItem {
-        private String description;
-        private boolean isDone;
+    private String description;
+    private boolean isDone;
 
-        public ToDoItem(String description)
-        {
-            this.description = description;
-            isDone = false;
-        }
-
-        public String GetDescription()
-        {
-            return description;
-        }
-
-        public void SetDescription(String description)
-        {
-            this.description = description;
-        }
-
-        public Boolean GetisDone()
-        {
-            return isDone;
-        }
-
-        public void SetisDone(boolean isDone) {
-            this.isDone = isDone;
-        }
-
-        // TODO Now:
-        // Add a constructor to initialize the item with the description, and isDone as false, with a single parameter for the description
-        // Add getters and setters for each field
+    public ToDoItem(String description)
+    {
+        this.description = description;
+        isDone = false;
     }
+
+    public String GetDescription()
+    {
+        return description;
+    }
+
+    public void SetDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public Boolean GetisDone()
+    {
+        return isDone;
+    }
+
+    public void SetisDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    // TODO Now:
+    // Add a constructor to initialize the item with the description, and isDone as false, with a single parameter for the description
+    // Add getters and setters for each field
 }
